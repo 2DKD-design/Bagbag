3f6f924df4aa9505c9330c06cae11f572866d6c5
Noel <noelnek@gmail.com>
2026-06-15 01:48:10 +0500

    updated site

--- Scripts/Carousel.js
+++ Scripts/Carousel.js
@@ -18,4 +18,13 @@ document.addEventListener("DOMContentLoaded", () => {
       setInterval(advanceSlide, displayDuration);
     });
 
+    $(document).ready(function(){
+        new Typed('.text-animate', {
+          strings: ['Leather', 'Luxury', 'Goods', 'Masterpieces'],
+          typeSpeed: 90,
+          backSpeed: 50,
+          showCursor: false,
+          loop: true  
+        })
+      })
 
--- Scripts/Nav.js
+++ Scripts/Nav.js
@@ -9,3 +9,35 @@
         document.getElementById("mySidenav").style.width = "0";
         document.body.style.backgroundColor = "white";
       } 
+
+      const Nav = document.querySelector('.navbar');
+
+      // Triggers when the mouse moves over the element
+      Nav.addEventListener('mouseenter', () => {
+        Nav.classList.remove('navbar-dark');
+        Nav.classList.add('navbar-light');
+      });
+
+      // Triggers when the mouse leaves the element
+      Nav.addEventListener('mouseleave', () => {
+        Nav.classList.remove('navbar-light');
+        Nav.classList.add('navbar-dark');
+      });
+
+      document.onscroll = function() {myFunction()};
+
+      function myFunction() {
+        let Nav = document.querySelector('.navbar')
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+          Nav.classList.remove('bg-transparent');
+          Nav.classList.remove('navbar-dark');
+          Nav.classList.add('bg-light');
+          Nav.classList.add('navbar-light');
+
+        }  else {
+          Nav.classList.add('bg-transparent');
+          Nav.classList.add('navbar-dark');
+          Nav.classList.remove('bg-light');
+          Nav.classList.remove('navbar-light');
+        }
+      }
